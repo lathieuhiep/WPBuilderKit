7d15ae0ead8c2b508344cdebd5950e6bde64069e
diep.lk <diep.lk@kiotviet.com>
2026-01-06 10:43:31 +0700

    update theme

--- themes/basictheme/header.php
+++ themes/basictheme/header.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php bloginfo('description'); ?>" />
-
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- themes/basictheme/single.php
+++ themes/basictheme/single.php
@@ -1,7 +1,7 @@
 <?php
-
-
 use ExtendSite\Admin\Options\Modules\SinglePostOptions;
+
+defined('ABSPATH') || exit;
 
 get_header();
 

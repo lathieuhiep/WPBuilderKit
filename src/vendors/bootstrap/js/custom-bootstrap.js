35582e284909b734de697492e6e5e73276ef355b
diep.lk <diep.lk@kiotviet.com>
2026-01-22 18:53:48 +0700

    fix build bootrap js

--- src/vendors/bootstrap/js/custom-bootstrap.js
+++ src/vendors/bootstrap/js/custom-bootstrap.js
@@ -2,7 +2,9 @@ import Modal from 'bootstrap/js/dist/modal';
 import Collapse from 'bootstrap/js/dist/collapse';
 import Offcanvas from 'bootstrap/js/dist/offcanvas';
 
-// Gán vào đối tượng window
-window.Modal = Modal;
-window.Offcanvas = Offcanvas;
-window.Collapse = Collapse;
+// Gom tất cả vào một đối tượng bootstrap toàn cục
+window.bootstrap = {
+    Modal: Modal,
+    Collapse: Collapse,
+    Offcanvas: Offcanvas
+}

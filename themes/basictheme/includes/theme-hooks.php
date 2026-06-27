c1939d9401f6e8b0e52cd270c3dea4ed87f87d45
lathieuhiep <khacdiepkma90@gmail.com>
2026-06-27 20:31:15 +0700

    remove action gutebug

--- themes/basictheme/includes/theme-hooks.php
+++ themes/basictheme/includes/theme-hooks.php
@@ -76,15 +76,6 @@ add_action('wp_footer', 'basictheme_custom_footer_code');
 // disable WordPress xmlrpc
 add_filter('xmlrpc_enabled', '__return_false');
 
-// disable gutenberg editor
-add_filter("use_block_editor_for_post_type", "basictheme_disable_gutenberg_editor");
-function basictheme_disable_gutenberg_editor(): bool {
-	return false;
-}
-
-// disable gutenberg widgets
-add_filter('use_widgets_block_editor', '__return_false');
-
 /**
  * Thêm thuộc tính preconnect cho Google Fonts
  */
